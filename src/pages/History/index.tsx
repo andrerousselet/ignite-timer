087cc56d8dd26ef2e95cddd5faee10ab672960f8
Andre Rousselet <andre098@hotmail.com>
2023-06-19 09:37:58 -0300

    feat(status component): add Status styled component with dynamic props

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
   return (
@@ -20,37 +20,49 @@ export function History() {
               <td>Exemplo de tarefa</td>
               <td>20 minutos</td>
               <td>Há cerca de 1 mês</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
             </tr>
             <tr>
               <td>Exemplo de tarefa</td>
               <td>20 minutos</td>
               <td>Há cerca de 1 mês</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
             </tr>
             <tr>
               <td>Exemplo de tarefa</td>
               <td>20 minutos</td>
               <td>Há cerca de 1 mês</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
             </tr>
             <tr>
               <td>Exemplo de tarefa</td>
               <td>20 minutos</td>
               <td>Há cerca de 1 mês</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
             </tr>
             <tr>
               <td>Exemplo de tarefa</td>
               <td>20 minutos</td>
               <td>Há cerca de 1 mês</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Exemplo de tarefa</td>
               <td>20 minutos</td>
               <td>Há cerca de 1 mês</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
             </tr>
           </tbody>
         </table>
--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -24,11 +24,11 @@ export const HistoryList = styled.div`
 
     th {
       background-color: ${props => props.theme["gray-600"]};
-      padding: 1rem;
-      text-align: left;
       color: ${props => props.theme["gray-100"]};
       font-size: 0.875rem;
       line-height: 1.6;
+      padding: 1rem;
+      text-align: left;
 
       &:first-child {
         border-top-left-radius: 8px;
@@ -44,9 +44,9 @@ export const HistoryList = styled.div`
     td {
       background-color: ${props => props.theme["gray-700"]};
       border-top: 4px solid ${props => props.theme["gray-800"]};
-      padding: 1rem;
       font-size: 0.875rem;
       line-height: 1.6;
+      padding: 1rem;
 
       &:first-child {
         padding-left: 1.5rem;
@@ -57,5 +57,29 @@ export const HistoryList = styled.div`
         padding-right: 1.5rem;
       }
     }
+  }
+`;
+
+const STATUS_COLORS = {
+  yellow: "yellow-500",
+  green: "green-500",
+  red: "red-500",
+} as const
+
+interface StatusProps {
+  statusColor: keyof typeof STATUS_COLORS;
+}
+
+export const Status = styled.span<StatusProps>`
+  align-items: baseline;
+  display: flex;
+  gap: 0.5rem;
+
+  &::before {
+    content: "";
+    background-color: ${props => props.theme[STATUS_COLORS[props.statusColor]]};
+    border-radius: 50%;
+    height: 0.5rem;
+    width: 0.5rem;
   }
 `;
